4f31a589bc9ff164bbef08ad5857af133a729f50
hibarifc <bankho181@gmail.com>
2017-06-14 11:45:30 +0700

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -42,4 +42,30 @@ router.post('/login', function (req, res, next) {
   }
 });
 
+
+//get regis user
+router.post('/register', function (req, res, next) {
+  let dbPool = req.dbPool;
+  let username = req.body.username;
+  let password = req.body.password;
+  let fullname = req.body.fullname;
+  let encryptedPassword = crypto.createHash('md5').update(password).digest('hex');
+  if (username && password) {
+    // Save user
+    User.save(dbPool,username,encryptedPassword,fullname)
+      .then((rows) => {
+        let user = {
+          username: username,
+          password: password,
+          fullname: fullname,
+        };
+        res.send({ ok: true, user: user });
+      }, (error) => {
+        res.send({ ok: false, error: error });
+      });
+  } else {
+    res.send({ ok: false, error: 'Data incomplete' });
+  }
+
+});
 module.exports = router;
